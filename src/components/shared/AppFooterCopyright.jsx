d2d8e38edb23e556a123278f27a1faa6b0b56455
fahim <333CCCorruption>
2023-07-23 22:20:18 +0400

    new mandatory commit

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -4,7 +4,7 @@ function AppFooterCopyright() {
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
 				&copy; {new Date().getFullYear()}
 				<a
-					href=""
+					href="v"
 					target="__blank"
 					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
